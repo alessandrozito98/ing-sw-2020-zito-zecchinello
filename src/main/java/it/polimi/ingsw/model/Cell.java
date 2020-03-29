d8a0eda2b604b6d892f99df8f9509b50c3b524ad
FlavioZecchinello <61889311+FlavioZecchinello@users.noreply.github.com>
2020-03-29 22:37:08 +0200

    cell modificata in accordo all'UML

--- src/main/java/it/polimi/ingsw/model/Cell.java
+++ src/main/java/it/polimi/ingsw/model/Cell.java
@@ -5,23 +5,15 @@ public class Cell {
     private int x;
     private int y;
     private boolean occupied;
-    private Block level1, level2, level3, dome;
+    private Block level;
     private Worker worker;
-    private boolean north = false;
-    private boolean south = false;
-    private boolean east = false;
-    private boolean west = false;
 
-
-    public Cell(int x, int y, boolean occupied, Block level1, Block level2, Block level3, Block dome, Worker worker) {
+    public Cell(int x, int y) {
         this.x = x;
         this.y = y;
-        this.occupied = occupied;
-        this.level1 = level1;
-        this.level2 = level2;
-        this.level3 = level3;
-        this.dome = dome;
-        this.worker = worker;
+        this.occupied = false;
+        this.level = null;
+        this.worker = null;
     }
 
     public int getX() {
@@ -32,37 +24,23 @@ public class Cell {
         return y;
     }
 
-    public boolean isNorth() {
-        return north;
-    }
+    public void build() {
+        if (level == null) {
+            level = Block.LEVEL1;
+        } else {
+            if (level == Block.LEVEL1) {
+                level = Block.LEVEL2;
+            } else {
+                if (level == Block.LEVEL2) {
+                    level = Block.LEVEL3;
+                } else {
+                    if (level == Block.LEVEL3) {
+                        level = Block.DOME;
+                    }
+                }
+            }
+        }
 
-    public void setNorth(boolean north) {
-        this.north = north;
-    }
-
-    public boolean isSouth() {
-        return south;
-    }
-
-    public void setSouth(boolean south) {
-        this.south = south;
-    }
-
-
-    public boolean isEast() {
-        return east;
-    }
-
-    public void setEast(boolean east) {
-        this.east = east;
-    }
-
-    public boolean isWest() {
-        return west;
-    }
-
-    public void setWest(boolean west) {
-        this.west = west;
     }
 
     public void addWorker(Worker worker) {
@@ -75,27 +53,16 @@ public class Cell {
 
     public void removeWorker() {
         this.worker = null;
+        occupied = false;
+    }
+
+    public boolean getOccupied(){
+        return this.occupied;
+    }
+
+    public Block getLevel(){
+        return this.level;
     }
 
 
-    public void build() {
-        if (!occupied) {
-
-            if (level1 == null) {
-                level1 = Block.LEVEL1;
-            } else {
-                if (level2 == null) {
-                    level2 = Block.LEVEL2;
-                } else {
-                    if (level3 == null) {
-                        level3 = Block.LEVEL3;
-                    } else {
-                        if (dome == null) {
-                            dome = Block.DOME;
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
