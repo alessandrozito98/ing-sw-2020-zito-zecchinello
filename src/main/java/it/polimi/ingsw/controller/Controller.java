667d5ba504016fa3fdb737b3ab0c3c3a5d02116d
FlavioZecchinello <61889311+FlavioZecchinello@users.noreply.github.com>
2020-05-01 11:01:33 +0200

    aggiunti metodi vuoti a controller

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -2,10 +2,11 @@ package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Game;
 import it.polimi.ingsw.observer.Observer;
+import it.polimi.ingsw.observer.messages.*;
 
 import java.util.Observable;
 
-public class Controller implements Observer<Game> {
+public class Controller implements Observer {
 
     private final Game game;
 
@@ -23,7 +24,32 @@ public class Controller implements Observer<Game> {
 
 
     @Override
-    public void update(Game message) {
+    public void updateMoveRequest(MoveRequest message) {
+        //QUA FA QUALCOSA
+    }
+
+    @Override
+    public void updateBuildRequest(BuildRequest message) {
+        //QUA FA QUALCOSA
+    }
+
+    @Override
+    public void updateEndTurnRequest(EndTurnRequest message) {
+        //QUA FA QUALCOSA
+    }
+    //I PROSSIMI UPDATE NON LI USERA' MAI MA BISOGNA "IMPLEMENTARLI" TUTTI PER FORZA
+    @Override
+    public void updateBoardChange(BoardChange message) {
+
+    }
+
+    @Override
+    public void updateStartTurn(StartTurn message) {
+
+    }
+
+    @Override
+    public void updatePlayerLose(PlayerLose message) {
 
     }
 }
