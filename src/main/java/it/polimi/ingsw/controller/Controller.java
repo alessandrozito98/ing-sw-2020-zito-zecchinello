70a4db5353dd39c7a65333750c176850bb5b3302
Alessandro Zito <alessandro4.zito@mail.polimi.it>
2020-05-04 17:15:29 +0200

    Fix

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -16,7 +16,7 @@ public class Controller implements Observer {
     }
 
     // crea un ArrayList che identifica le celle su cui un worker può fare una move
-    public ArrayList<Cell> availableMoveCells(int playerNumber, int workerNumber, Board board){
+    public ArrayList<Cell> checkMovement(int playerNumber, int workerNumber, Board board){
         ArrayList<Cell> availableMoveCells = new ArrayList<Cell>();
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
@@ -29,7 +29,7 @@ public class Controller implements Observer {
     }
 
     // crea un ArrayList che identifica le celle su cui un worker può fare una build
-    public ArrayList<Cell> availableBuildCells(int playerNumber, int workerNumber, Board board){
+    public ArrayList<Cell> checkBuilding(int playerNumber, int workerNumber, Board board){
         ArrayList<Cell> availableBuildCells = new ArrayList<Cell>();
         Level level;
         for(int i=0; i<5; i++){
@@ -46,13 +46,13 @@ public class Controller implements Observer {
 
 
     public synchronized void handleMove(MoveRequest message) {
-        if(availableMoveCells(message.getPlayerNumber(), message.getWorkerNumber(), game.getBoardCopy()).size() != 0) {
+        if(checkMovement(message.getPlayerNumber(), message.getWorkerNumber(), game.getBoardCopy()).size() != 0) {
             game.performMove(message.getPlayerNumber(), message.getWorkerNumber(), message.getxPosition(), message.getyPosition());
         }
     }
 
     public synchronized void handleBuild(BuildRequest message) {
-        if(availableBuildCells(message.getPlayerNumber(),message.getWorkerNumber(), game.getBoardCopy()).size() != 0) {
+        if(checkBuilding(message.getPlayerNumber(),message.getWorkerNumber(), game.getBoardCopy()).size() != 0) {
             game.performBuild(message.getPlayerNumber(),message.getWorkerNumber(),message.getxPosition(), message.getyPosition(), message.getLevel());
         }
     }
